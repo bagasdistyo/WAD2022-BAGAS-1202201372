78f8f1583216c52d2e7c99b83066d476842981ac
alfianmf <alfianm999@gmail.com>
2022-11-04 07:23:48 +0700

    debug

--- MODUL 2 BAGAS/index.php
+++ MODUL 2 BAGAS/index.php
@@ -1,98 +1,98 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Praktikum Modul 2</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-  </head>
-  <style>
-    p { font-size: 14px;}
-  </style>
+    <!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Praktikum Modul 2</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+      </head>
+      <style>
+        p { font-size: 14px;}
+      </style>
 
-<body>
-<nav>
-<ul class="nav justify-content-center" style="background-color: black;">
-    <li class="nav-item">
-      <a class="nav-link active" aria-current="page" href="index.php" style="color:white">Home</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link active" href="menu_booking.php" style="color:white">Booking</a>
-    </li>
-</ul>
-</nav>
+    <body>
+    <nav>
+    <ul class="nav justify-content-center" style="background-color: black;">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php" style="color:white">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="menu_booking.php" style="color:white">Booking</a>
+        </li>
+    </ul>
+    </nav>
 
-<section align="center">
-    <div class="container">
-        <p>&nbsp</p>
-        <h3>WELCOME TO EAD RENT</h3>
-        <p>Find your best deal, here !</p>
-        <p>&nbsp</p>
-        <table align="center">
-            <tr>
-                <td>
-                  <div class="card" style="width: 18rem;">
-                    <img src="Alphardd.png" class="card-img-top" alt="Toyota Alphard" style="width: 250px; height: 160px;">
-                    <div class="card-body">
-                      <h6 class="card-title" align="left">Toyota Alphard</h6>
-                      <p class="card-text" align="left">Rp. 1.000.000 / Day</p>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                      <li class="list-group-item" style="color:blue">10 Kursi</li>
-                      <li class="list-group-item" style="color:blue">1600 cc</li>
-                      <li class="list-group-item" style="color:blue">Matic</li>
-                    </ul>
-                    <div class="card-body" style="background-color: ghostwhite;">
-                      <a href="menu_booking.php" class="btn btn-primary">Book Now</a>
-                    </div>
-                  </div>
-                </td>
-                <td width="70px"></td>
-                <td>
-                  <div class="card" style="width: 18rem;" >
-                    <img src="Ferrari 458.jpg" class="card-img-top" alt="Ferrari 458" style="width: 250px; height: 160px;">
-                    <div class="card-body">
-                      <h6 class="card-title" align="left">Ferrari 458</h6>
-                      <p class="card-text" align="left">Rp. 3.000.000 / Day</p>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                      <li class="list-group-item" style="color:blue">4 Kursi</li>
-                      <li class="list-group-item" style="color:blue">2500 cc</li>
-                      <li class="list-group-item" style="color:blue">Matic</li>
-                    </ul>
-                    <div class="card-body" style="background-color: ghostwhite;">
-                      <a href="menu_booking.php" class="btn btn-primary">Book Now</a>
-                    </div>
-                  </div>
-                </td>
-                <td width="70px"></td>
-                <td>
-                  <div class="card" style="width: 18rem;">
-                    <img src="Toyota Supra.jpg" class="card-img-top" alt="Toyota Supra" style="width: 250px; height: 160px;">
-                    <div class="card-body">
-                      <h6 class="card-title" align="left">Toyota Supra</h6>
-                      <p class="card-text" align="left">Rp. 11.000.000 / Day</p>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                      <li class="list-group-item" style="color:blue">2 Kursi</li>
-                      <li class="list-group-item" style="color:blue">3000 cc</li>
-                      <li class="list-group-item" style="color:blue">Manual</li>
-                    </ul>
-                    <div class="card-body" style="background-color: ghostwhite;">
-                      <a href="menu_booking.php" class="btn btn-primary">Book Now</a>
-                    </div>
-                  </div>
-                </td>
-            </tr>
-        </table>
-    </div>
-    <footer>
-        <p>&nbsp</p>
-        <p align="center" style="background-color: ghostwhite;">Created by BAGAS DISTYO UTOMO_1202201372</p>
-    </footer>
-</section>
+    <section align="center">
+        <div class="container">
+            <p>&nbsp</p>
+            <h3>WELCOME TO EAD RENT</h3>
+            <p>Find your best deal, here !</p>
+            <p>&nbsp</p>
+            <table align="center">
+                <tr>
+                    <td>
+                      <div class="card" style="width: 18rem;">
+                        <img src="Alphardd.png" class="card-img-top" alt="Toyota Alphard" style="width: 250px; height: 160px;">
+                        <div class="card-body">
+                          <h6 class="card-title" align="left">Toyota Alphard</h6>
+                          <p class="card-text" align="left">Rp. 1.000.000 / Day</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                          <li class="list-group-item" style="color:blue">10 Kursi</li>
+                          <li class="list-group-item" style="color:blue">1600 cc</li>
+                          <li class="list-group-item" style="color:blue">Matic</li>
+                        </ul>
+                        <div class="card-body" style="background-color: ghostwhite;">
+                          <a href="menu_booking.php" class="btn btn-primary">Book Now</a>
+                        </div>
+                      </div>
+                    </td>
+                    <td width="70px"></td>
+                    <td>
+                      <div class="card" style="width: 18rem;" >
+                        <img src="Ferrari 458.jpg" class="card-img-top" alt="Ferrari 458" style="width: 250px; height: 160px;">
+                        <div class="card-body">
+                          <h6 class="card-title" align="left">Ferrari 458</h6>
+                          <p class="card-text" align="left">Rp. 3.000.000 / Day</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                          <li class="list-group-item" style="color:blue">4 Kursi</li>
+                          <li class="list-group-item" style="color:blue">2500 cc</li>
+                          <li class="list-group-item" style="color:blue">Matic</li>
+                        </ul>
+                        <div class="card-body" style="background-color: ghostwhite;">
+                          <a href="menu_booking.php" class="btn btn-primary">Book Now</a>
+                        </div>
+                      </div>
+                    </td>
+                    <td width="70px"></td>
+                    <td>
+                      <div class="card" style="width: 18rem;">
+                        <img src="Toyota Supra.jpg" class="card-img-top" alt="Toyota Supra" style="width: 250px; height: 160px;">
+                        <div class="card-body">
+                          <h6 class="card-title" align="left">Toyota Supra</h6>
+                          <p class="card-text" align="left">Rp. 11.000.000 / Day</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                          <li class="list-group-item" style="color:blue">2 Kursi</li>
+                          <li class="list-group-item" style="color:blue">3000 cc</li>
+                          <li class="list-group-item" style="color:blue">Manual</li>
+                        </ul>
+                        <div class="card-body" style="background-color: ghostwhite;">
+                          <a href="menu_booking.php" class="btn btn-primary">Book Now</a>
+                        </div>
+                      </div>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <footer>
+            <p>&nbsp</p>
+            <p align="center" style="background-color: ghostwhite;">Created by BAGAS DISTYO UTOMO_1202201372</p>
+        </footer>
+    </section>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-  </body>
-</html>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+      </body>
+    </html>
