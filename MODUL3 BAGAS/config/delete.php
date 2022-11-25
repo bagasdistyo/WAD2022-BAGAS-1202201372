c09e08ffc0fde4b65ff3db967ae18a8d563ec91b
alfianmf <alfianm999@gmail.com>
2022-11-25 08:33:04 +0700

    debug

--- MODUL3 BAGAS/config/delete.php
+++ MODUL3 BAGAS/config/delete.php
@@ -1,12 +1,12 @@
-<?php
-include('..\config\connector.php');
+        <?php
+        ('..\config\connector.php');
 
-$id_mobil=$_GET['id_mobil'];
-$query="DELETE FROM modul3 WHERE id_mobil=$id_mobil";
-mysqli_query($kon, $query);
-if ($query){
-    header('Location: ..\pages\ListCar-Bagas.php');
-}
+        $id_mobil=$_GET['id_mobil'];
+        $query="DELETE FROM modul3 WHERE id_mobil=$id_mobil";
+        mysqli_query($kon, $query);
+        if ($query){
+            header('Location: ..\pages\ListCar-Bagas.php');
+        }
 
 
-?>
+        ?>
--- MODUL3 BAGAS/config/edit.php
+++ MODUL3 BAGAS/config/edit.php
@@ -1,26 +1,26 @@
-<?php
+        <?php
 
-include('..\config\connector.php');
+        include('..\config\connector.php');
 
 
-if (isset($_GET['edit'])){
-    $id_mobil =$_GET['id_mobil'];
-    $nama_mobil=$_GET['nama_mobil'];
-    $pemilik_mobil=$_GET['pemilik_mobil'];
-    $merk_mobil=$_GET['merk_mobil'];
-    $tanggal_beli=$_GET['tanggal_beli'];
-    $deskripsi=$_GET['deskripsi'];
+        if (isset($_GET['edit'])){
+            $id_mobil =$_GET['id_mobil'];
+            $nama_mobil=$_GET['nama_mobil'];
+            $pemilik_mobil=$_GET['pemilik_mobil'];
+            $merk_mobil=$_GET['merk_mobil'];
+            $tanggal_beli=$_GET['tanggal_beli'];
+            $deskripsi=$_GET['deskripsi'];
 
-    $foto_mobil=$_FILES['foto_mobil']["name"];
-    $temp = $_FILES["foto_mobil"]["tmp_name"];
-    $folder = "../asset/image/" . $foto_mobil;
-    move_uploaded_file($temp, '../asset/image/'.$foto_mobil);
-    $status_pembayaran=$_GET['status_pembayaran'];
+            $foto_mobil=$_FILES['foto_mobil']["name"];
+            $temp = $_FILES["foto_mobil"]["tmp_name"];
+            $folder = "../asset/image/" . $foto_mobil;
+            move_uploaded_file($temp, '../asset/image/'.$foto_mobil);
+            $status_pembayaran=$_GET['status_pembayaran'];
 
-    $query = mysqli_query($kon,"UPDATE modul3 set id_mobil='$id_mobil', nama_mobil='$nama_mobil', pemilik_mobil='$pemilik_mobil', merk_mobil='$merk_mobil', tanggal_beli='$tanggal_beli', deskripsi='$deskripsi', foto_mobil='$foto_mobil', status_pembayaran='$status_pembayaran' where id_mobil='id_mobil'");
+            $query = mysqli_query($kon,"UPDATE modul3  id_mobil='$id_mobil', nama_mobil='$nama_mobil', pemilik_mobil='$pemilik_mobil', merk_mobil='$merk_mobil', tanggal_beli='$tanggal_beli', deskripsi='$deskripsi', foto_mobil='$foto_mobil', status_pembayaran='$status_pembayaran' where id_mobil='id_mobil'");
 
-    if ($query){
-        header('Location: ..\pages\Edit-Bagas.php');
-    } 
-}
-?>
+            if ($query){
+                header('Location: ..\pages\Edit-Bagas.php');
+            } 
+        }
+        ?>
